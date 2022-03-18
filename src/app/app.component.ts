36b827522a71818037a4f6f7f49ffdd5f518c0b1
Jank1510 <89560208+Jank1510@users.noreply.github.com>
2022-03-17 21:21:46 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,89 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rock-paper-scissors-master';
+
+  eleccion: boolean = true
+  proceso: boolean = false
+  rules: boolean = false
+  opacity!: string
+  seleccion!: string
+  clase!: string
+  cargandoRespuesta!: boolean
+  respuestaCargada!: boolean
+  EleccionMaquinaClase!:string
+  EleccionMaquinaIMG!:string
+
+  closedRules() {
+    this.opacity = 'opacityClosed'
+    setTimeout(() => {
+      this.rules = false
+    }, 500);
+  }
+  openRules() {
+    this.opacity = 'opacityOpen'
+    this.rules = true
+  }
+  /*metodos de eleccion*/
+  papel() {
+    this.eleccion = false
+    this.proceso = true
+    this.seleccion = '../assets/icon-paper.svg'
+    this.clase = 'papel'
+    this.cargandoRespuesta = true
+    this.opacity = 'opacityOpen'
+    this.getRandomIntInclusive(1,3)
+    
+
+  }
+  piedra() {
+    this.eleccion = false
+    this.proceso = true
+    this.seleccion = '../assets/icon-rock.svg'
+    this.clase = 'piedra'
+    this.cargandoRespuesta = true
+    this.opacity = 'opacityOpen'
+
+  }
+  tijera() {
+    this.eleccion = false
+    this.proceso = true
+    this.clase = 'tijera'
+    this.cargandoRespuesta = true
+    this.opacity = 'opacityOpen'
+    this.seleccion = '../assets/icon-scissors.svg'
+  }
+  getRandomIntInclusive(min: any, max: any) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    var randomValor = Math.floor(Math.random() * (max - min + 1)) + min; // El máximo y el mínimo son inclusivos.
+    if (randomValor == 1) {
+      console.log('papel')
+      setTimeout(() => {
+        this.EleccionMaquinaClase='papel'
+        this.EleccionMaquinaIMG='../assets/icon-paper.svg'
+        this.cargandoRespuesta=false
+        this.respuestaCargada=true
+      }, 1500);
+    } else {
+      if (randomValor == 2) {
+        console.log('piedra')
+        setTimeout(() => {
+          this.EleccionMaquinaClase='piedra'
+          this.EleccionMaquinaIMG='../assets/icon-rock.svg'
+          this.cargandoRespuesta=false
+          this.respuestaCargada=true
+        }, 1500);
+      } else {
+        if (randomValor == 3) {
+          console.log('tijera')
+          setTimeout(() => {
+            this.EleccionMaquinaClase='tijera'
+            this.EleccionMaquinaIMG='../assets/icon-scissors.svg'
+            this.cargandoRespuesta=false
+            this.respuestaCargada=true
+          }, 1500);
+        }
+      }
+    }
+  }
 }
